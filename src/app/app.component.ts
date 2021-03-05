a2c5f8770d1fc9a7e6748b25cbdcbb3eda4007ed
user name <user email>
2021-03-05 21:36:39 +0530

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+declare let $: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  ngOnInit() {
+    $(document).foundation();
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  STREAM_APP_ID: '29337',
+  STREAM_APP_KEY: 'pvjukxquhwfy',
+  STREAM_FEED_GROUP: 'framework_feeds',
+  STREAM_FEED_ID: 'angular',
+  STREAM_FEED_READ_ONLY_TOKEN:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+    'eyJyZXNvdXJjZSI6IioiLCJhY3Rpb24iOiJyZW' +
+    'FkIiwiZmVlZF9pZCI6ImZyYW1ld29ya19mZWVk' +
+    'c2FuZ3VsYXIifQ.vv0tJHc-B9ojS4CyN2tUxJZ' +
+    'yNdu58LtN_CsBXEHPwfc'
+};
